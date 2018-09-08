24e62a65c8ad1a687edece60f2b6f6044d65ed4f
Bruce McElroy <bruce.mcelroy@gmail.com>
2018-09-07 21:43:43 -0400

    updated some stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,7 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+    <div class="home">
+        <HelloWorld />
+    </div>
 </template>
 
 <script lang="ts">
@@ -10,9 +9,10 @@ import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 @Component({
-  components: {
-    HelloWorld,
-  },
+    components: {
+        HelloWorld
+    }
 })
-export default class Home extends Vue {}
+export default class Home extends Vue {
+}
 </script>
