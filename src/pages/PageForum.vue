3d22d6d4cc303d69ecd4272130efec669951486e
Bruce McElroy <bruce.mcelroy@gmail.com>
2018-09-15 16:34:56 -0400

    organize threads into forums

---
+++ src/pages/PageForum.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="page-forum">
+        <div class="forum-header">
+            <div class="forum-details">
+                <h1>Cooking</h1><p class="text-lead">Discuss your passion for fod and cooking</p>
+            </div>
+            <a href="#" class="btn-green btn-small">Start a thread</a>
+        </div>
+        <div class="col-full push-top">
+            <ThreadList :threads="threads"/>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+    import { Component, Prop, Vue } from 'vue-property-decorator';
+    import ThreadList from '@/components/ThreadList.vue';
+    import sourceData from '@/data.json';
+
+    @Component({
+        name: 'PageForum',
+        components: {
+            ThreadList
+        }
+    })
+    export default class PageForum extends Vue {
+        @Prop({ required: true, type: String }) public id: string;
+
+        get forum() {
+            return sourceData.forums[this.id];
+        }
+
+        get threads() {
+            return Object.values(sourceData.threads)
+                .filter(thread => thread.forumId === this.id);
+        }
+    };
+</script>
+
+<style scoped>
+    .page-forum {
+        width: 100%;
+    }
+</style>
--- src/router/router.ts
+++ src/router/router.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '@/pages/PageHome.vue';
+import PageForum from '@/pages/PageForum.vue';
 import PageThreadShow from '@/pages/PageThreadShow.vue';
 import PageNotFound from '@/pages/PageNotFound.vue';
 
@@ -16,6 +17,12 @@ const routes = [
         path: '/thread/:id',
         name: 'PageThreadShow',
         component: PageThreadShow,
+        props: true
+    },
+    {
+        path: '/forum/:id',
+        name: 'PageForum',
+        component: PageForum,
         props: true
     },
     {
